de456a8b29cd6f3ec3b12b1a33b729a4d54e954a
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-02-12 21:06:01 +0000

    added styling for input form

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -6,46 +6,46 @@ import '../assets/style/contact.css'
 function Contact() {
   return (
     <>
-     <div className='contact-container'>
-       <div className='contact-info'>
-       <h2> Check me out!</h2>
-          <p1> Email: k.kierepka.a@gmail.com</p1> 
+      <div className='contact-container'>
+        <div className='contact-info'>
+          <h2> Check me out!</h2>
+          <p1> Email: k.kierepka.a@gmail.com</p1>
           <p1> (or you can use form below!)</p1>
           <p2>Mobile: 07930997665</p2>
-          <a href='https://www.linkedin.com/in/klaudia-kierepka' target='blank' className='link'>LinkedIn</a> 
+          <a href='https://www.linkedin.com/in/klaudia-kierepka' target='blank' className='link'>LinkedIn</a>
           <a href='https://github.com/klaudia102' target='blank' className='link' >GitHub</a>
           <a href='' target='blank' className='link'>Download my CV</a>
-       </div>
-        <div className='contact-form'>
-          <form nameClass="row g-3 needs-validation" novalidate>
-            <div nameClass="col-md-4 position-relative">
-              <label for="validationTooltip01" nameClass="form-label">First name</label>
-              <input type="text" nameClass="form-control" id="validationTooltip01" value="Mark" required />
+        </div>
+        {/* contact input */}
+        <div className='contact-form-container'>
+          <form nameClass="row g-3 needs-validation " novalidate>
+
+            <div nameClass="col-md-4 position-relative contact-form input1">
+              <input type="text" nameClass="form-control" id="validationTooltip01" placeholder="Name" required />
               <div nameClass="valid-tooltip">
               </div>
             </div>
-            <div nameClass="col-md-4 position-relative">
-              <label for="validationTooltip02" nameClass="form-label">Last name</label>
-              <input type="text" nameClass="form-control" id="validationTooltip02" value="Otto" required />
+            <div nameClass="col-md-4 position-relative contact-form input1">
+              <input type="text" nameClass="form-control" id="validationTooltip02" placeholder="Surname" required />
               <div nameClass="valid-tooltip">
               </div>
             </div>
-            <div nameClass="col-md-4 position-relative">
-              <label for="validationTooltipUsername" nameClass="form-label">Email</label>
+            
+            <div nameClass="col-md-4 position-relative contact-form input1">
               <div nameClass="input-group has-validation">
                 <span nameClass="input-group-text" id="validationTooltipUsernamePrepend"></span>
                 <input type="text" nameClass="form-control"
-                  id="validationTooltipUsername" aria-describedby="validationTooltipUsernamePrepend" required />
+                  id="validationTooltipUsername" aria-describedby="validationTooltipUsernamePrepend"
+                   placeholder="Email" required />
                 <div nameClass="invalid-tooltip">
                 </div>
               </div>
+
             </div>
             <form nameClass="was-validated">
               <div nameClass="mb-3">
-                <label for="validationTextarea" nameClass="form-label">Textarea</label>
                 <textarea nameClass="form-control" id="validationTextarea" placeholder="Required example textarea" required></textarea>
                 <div nameClass="invalid-feedback">
-                  Please enter a message in the textarea.
                 </div>
               </div>
             </form>
@@ -54,7 +54,7 @@ function Contact() {
             </div>
           </form>
         </div>
-     </div>
+      </div>
     </>
   );
 }
