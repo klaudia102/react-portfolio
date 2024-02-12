adf27d20bdb3f3a5366f57a100d8b3fc63fec4d0
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-02-12 14:51:59 +0000

    added homepage and footer

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,12 @@ import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
 import ProjectGallery from './components/pages/ProjectGallery'
 import Header from './components/Header'
+import Footer from './components/footer'
 
 function App() {
 
   return (
+    <>
    <Router>
     <Header />
     <Routes>
@@ -20,6 +22,8 @@ function App() {
       < Route path="/gallery" element={<ProjectGallery />} />
     </Routes>
    </Router>
+   <Footer />
+   </>
   )
   }
 export default App
---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './assets/style/footer.css'
+
+function Footer() {
+    return <>
+        <div className="container">
+            <a href='https://www.linkedin.com/in/klaudia-kierepka'>LinkedIn</a>
+            <a href='https://github.com/klaudia102'>GitHub</a>
+        </div>
+    </>
+}
+
+export default Footer
