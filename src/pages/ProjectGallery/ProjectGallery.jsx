fea76d927e5d60e8dcfabdea4279ed564fc363e7
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-02-14 13:06:37 +0000

    styling for portfolio gallery

--- src/pages/ProjectGallery/ProjectGallery.jsx
+++ src/pages/ProjectGallery/ProjectGallery.jsx
@@ -7,26 +7,25 @@ function ProjectGallery() {
     return (
         <>
             <div className='container-gallery'>
-                <div>
-                    <h1>My projects</h1>
-                    <h3>Would you like to see your dream website here...?</h3>
-                    
+                <div className="container-fluid py-5 jumboCopy">
+                    <h1 className="display-5 fw-bold">My projects</h1>
+                    <h2>Would you like to see your dream website here...?</h2>
                 </div>
                 <div className='gallery-space'>
-                    <div className='gallery-items'>
-
-                        <div class="card gallery-card">
-                            <img src="..." className="card-img-top" alt="..." /> 
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text 
+                    <div class="card gallery-card">
+                        <img src="..." className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text
                                 to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <div button-container>
+                                <a href="#" className="btn btn-primary deploy">Webside</a>
+                                <a href="#" className="btn btn-primary repo">Repo</a>
                             </div>
-                         </div> 
+                        </div>
                     </div>
                 </div>
+
             </div>
         </>
     )
