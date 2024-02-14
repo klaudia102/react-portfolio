981216cd188b75e176322caeb755de3f5529f346
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-02-14 13:53:11 +0000

    added projects array

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
-import './project.css'
+
 
 // A list of projects (Challenges or Projects). For each project, make sure you have the following:
 //  Project title
@@ -9,10 +9,22 @@ import './project.css'
 //  GIF or screenshot of the deployed application
 // Must render dynamically 6 instances of the Project component
 //  Be sure to store your project data in a JSON file and import it into your project
-function Project() {
+function Project(props) {
   return (
-  <>
-  </>
+    <>
+      <div class="card gallery-card">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text
+            to build on the card title and make up the bulk of the card's content.</p>
+          <div button-container>
+            <a href="#" target='blank' className="btn btn-primary deploy">Webside</a>
+            <a href="#" target='blank' className="btn btn-primary repo">Repo</a>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/pages/ProjectGallery/ProjectGallery.jsx
+++ src/pages/ProjectGallery/ProjectGallery.jsx
@@ -1,7 +1,53 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './ProjectGallery.css'
+import Project from '../../components/Project/Project';
+// A list of projects (Challenges or Projects). For each project, make sure you have the following:
+//  Project title
+//  Link to the deployed version
+//  Link to the GitHub repository
+//  GIF or screenshot of the deployed application
+// Must render dynamically 6 instances of the Project component
+//  Be sure to store your project data in a JSON file and import it into your project
 
+const projects = [
+    {
+        title: 'Wordsmith',
+        deploy:'https://klaudia102.github.io/WordSmith/',
+        repo: 'https://github.com/klaudia102/WordSmith',
+        screenshot: 'public/wordsmith',
+    },
+    {
+        title: 'Team Profile Generator',
+        deploy:'https://klaudia102.github.io/Team-Profile-Generator/',
+        repo: 'https://github.com/klaudia102/Team-Profile-Generator',
+        screenshot: 'public/team-profile-generator',
+    },
+    {
+        title: 'Generate ReadMe.md',
+        deploy:'https://klaudia102.github.io/Team-Profile-Generator/',
+        repo: 'https://github.com/klaudia102/generatorREADME.md',
+        screenshot: 'public/generate Readme.md',
+    },
+    {
+        title: 'password-generator',
+        deploy:'https://klaudia102.github.io/password-generator/',
+        repo: 'https://github.com/klaudia102/password-generator',
+        screenshot: 'public/password-generator',
+    },
+    {
+        title: 'Klaudia-portfolio',
+        deploy:'https://klaudia102.github.io/Klaudia-portfolio/',
+        repo: 'https://github.com/klaudia102/Klaudia-portfolio',
+        screenshot: 'public/Klaudia-portfolio',
+    },
+    {
+        title: 'Bootstrap-portfolio',
+        deploy:'https://klaudia102.github.io/Bootstrap-Portfolio/',
+        repo: 'https://github.com/klaudia102/Bootstrap-Portfolio',
+        screenshot: 'public/Bootstrap-Portfolio',
+    }
+]
 
 function ProjectGallery() {
     return (
@@ -12,7 +58,7 @@ function ProjectGallery() {
                     <h2>Would you like to see your dream website here...?</h2>
                 </div>
                 <div className='gallery-space'>
-                    <div class="card gallery-card">
+                    {/* <div class="card gallery-card">
                         <img src="..." className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">Card title</h5>
@@ -23,7 +69,7 @@ function ProjectGallery() {
                                 <a href="#" className="btn btn-primary repo">Repo</a>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
 
             </div>
