3852761b386cf91b4779346921ee0befe46e0b68
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-02-16 11:40:35 +0000

    added media query and gallery items

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -15,7 +15,7 @@ function Contact() {
           <div className='links'>
             <a href='https://www.linkedin.com/in/klaudia-kierepka' target='blank' className='link'>LinkedIn</a>
             <a href='https://github.com/klaudia102' target='blank' className='link' >GitHub</a>
-            <a href='' target='blank' className='link'>Download my CV</a>
+            <a href='public/Klaudia Kierepka-CV.pdf' target='blank' className='link'>Download my CV</a>
           </div>
         </div>
         {/* contact input */}
